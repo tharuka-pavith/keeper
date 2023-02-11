066e473b3061ebebfba00da99e91178936d39677
tharuka-pavith <tharukapavith2@gmail.com>
2023-02-11 23:04:18 +0530

    Add data to data.js

--- src/components/data.js
+++ src/components/data.js
@@ -3,7 +3,7 @@ const notes =[
   {title: "Jammy Jellyfish", 
   description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.laborum tempora magni ex quibusdam repellendus"},
   {title: "Focal Fossa", 
-  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.laborum tempora magni ex quibusdam repellendus"},
+  description: "LLorem ipsum dolor sit amet consectetur adipisicing elit.laborum tempora magni ex quibusdam repellendusorem ipsum dolor sit amet consectetur adipisicing elit.laborum tempora magni ex quibusdam repellendus Lorem ipsum dolor sit amet consectetur adipisicing elit.laborum tempora magni ex quibusdam repellendus Lorem ipsum dolor sit amet consectetur adipisicing elit.laborum tempora magni ex quibusdam repellendus"},
   {title: "Bionic Beaver", 
   description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.laborum tempora magni ex quibusdam repellendus"},
   {title: "Bionic Beaver", 
